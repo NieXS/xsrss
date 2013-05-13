afe4679392441cef7ef04be7ac454cb0133d66ba
NieXS <neo.niexs@gmail.com>
2013-05-13 20:26:24 -0300

    Now using waf instead of a build.sh

--- src/build.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-valac --pkg gee-0.8 --pkg libxml-2.0 --pkg posix --pkg gio-2.0 --pkg libsoup-2.4 --pkg sqlite3 --vapidir=../vapi -o xsrss -g feed.vala main.vala database.vala feedmanager.vala
